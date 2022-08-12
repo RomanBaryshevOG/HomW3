7c54e10e9124027aefb79eaf0db9a4a2d2072354
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-12 15:01:09 +0500

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+if (num > 0)
+
+   while (i <= num)
+   {
+       int c = i * i * i;
+       Console.WriteLine($"{i}  |  {c}");
+       i++;
+   }
